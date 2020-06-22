74ee895a175508861f198395652e0ef5b013d44e
clewsy <jadon@clews.pro>
2020-06-22 16:14:17 +1000

    Style and readability improvements.

--- rad10d.c
+++ rad10d.c
@@ -10,7 +10,7 @@ static struct mpd_connection *setup_connection(void)
 	struct mpd_connection *conn;					//Initialise the struct.
 
 	conn = mpd_connection_new(MPD_SERVER, MPD_PORT, MPD_TIMEOUT);	//Settings include mpd server IP address, port # and connection timeout (ms).
-	if (conn == NULL) {exit(EXIT_FAILURE);}				//Exit on failure to initialise mpd connection.
+	if (conn == NULL) exit(EXIT_FAILURE);				//Exit on failure to initialise mpd connection.
 	return(conn);							//Return the address of the connection struct.
 }
 
@@ -19,7 +19,7 @@ static struct mpd_connection *setup_connection(void)
 bool init_mpd(void)
 {
 	connection = setup_connection();			//Initialise mpd connection session.
-	if(connection == NULL) {return(FALSE);}			//Confirm "connection" was successfully set up.  If not, return false.
+	if(connection == NULL) return(FALSE);			//Confirm "connection" was successfully set up.  If not, return false.
 	mpd_connection_set_keepalive(connection, TRUE);		//Enable TCP keepalives to prevent disconnection in the event of a long idle duration.
 
 	return(TRUE);						//Apparently connection has been successfully set up so return true.
@@ -29,10 +29,10 @@ bool init_mpd(void)
 //Attempt to close then restablish an mpd connection.
 bool mpd_reconnect(void)
 {
-	mpd_connection_free(connection);		//Attempt to close the mpd connection and free the memory.
-	if (init_mpd() == FALSE) {return(FALSE);};	//Attempt to re-initialise a new (identical) mpd connection.
+	mpd_connection_free(connection);	//Attempt to close the mpd connection and free the memory.
+	if (init_mpd() == FALSE) return(FALSE);	//Attempt to re-initialise a new (identical) mpd connection.
 
-	return(TRUE);					//Connection should be successfully re-established so return true.
+	return(TRUE);				//Connection should be successfully re-established so return true.
 }
 
 
@@ -45,6 +45,7 @@ uint8_t get_mpd_status(void)
 {
 	struct mpd_status *status_struct = mpd_run_status(connection);	//Create a struct "status_struct" and fill it with status info of "connection".
 	int status = mpd_status_get_state(status_struct);		//Pull the unknown/stop/play/pause status from "status_struct".
+
 	while ((status < MPD_STATE_STOP) || (status > MPD_STATE_PAUSE))	//While the status returned is anything other than STOP, PLAY or PAUSE...
 	{
 		while (mpd_reconnect() == FALSE) {};			//Attempt re-establishing the mpd connection.
@@ -59,17 +60,18 @@ uint8_t get_mpd_status(void)
 //Function to initialise the struct that reflects the state of the encoder.
 struct encoder *init_encoder(uint8_t channel_a, uint8_t channel_b)
 {
-	the_encoder = malloc(sizeof(struct encoder));	//Allocates heap memory for globally accesible "the_encoder".
+	the_encoder = malloc(sizeof(struct encoder));	//Allocates heap memory for globally accessible "the_encoder".
 
 	the_encoder->channel_a = channel_a;	//Define the encoder channel a pin.
 	the_encoder->channel_b = channel_b;	//Define the encoder channel b pin.
 	the_encoder->last_code = 0;		//Initialise the last encoder code (for comparing to current code).
 	the_encoder->volume_delta = 0;		//Initialise the value of desired volume change.
 
-	if (gpioSetPullUpDown(channel_a, PI_PUD_UP) != 0) {return(NULL);}			//Enable pull-up resistor on channel a pin.
-	if (gpioSetPullUpDown(channel_b, PI_PUD_UP) != 0) {return(NULL);}			//Enable pull-up resistor on channel b pin.
-	if (gpioSetISRFunc(channel_a, EITHER_EDGE, 100, &volume_ISR) !=0) {return(NULL);}	//Enable hardware interrupt input on encoder channel a pin and define routine to run.
-	if (gpioSetISRFunc(channel_b, EITHER_EDGE, 100, &volume_ISR) !=0) {return(NULL);}	//Enable hardware interrupt input on encoder channel b pin and define routine to run.
+	if (gpioSetPullUpDown(channel_a, PI_PUD_UP)) return(NULL);	//Enable pull-up resistor on channel a pin.
+	if (gpioSetPullUpDown(channel_b, PI_PUD_UP)) return(NULL);	//Enable pull-up resistor on channel b pin.
+
+	if (gpioSetISRFunc(channel_a, EITHER_EDGE, 100, &volume_ISR)) return(NULL);	//Enable hardware interrupt input on encoder channel a pin and define routine to run.
+	if (gpioSetISRFunc(channel_b, EITHER_EDGE, 100, &volume_ISR)) return(NULL);	//Enable hardware interrupt input on encoder channel b pin and define routine to run.
 
 	return(the_encoder);	//Return address of "*the_encoder".
 }
@@ -79,11 +81,11 @@ struct encoder *init_encoder(uint8_t channel_a, uint8_t channel_b)
 //The gpioSetISRFunc requires ISRs such as this to receive gpio, level and tick.  These values are ignored in this use-case.
 void volume_ISR(int32_t gpio, int32_t level, uint32_t tick)
 {
-	bool MSB = gpioRead(the_encoder->channel_a);		//Define most-significant bit from 2-bit encoder.
-	bool LSB = gpioRead(the_encoder->channel_b);		//Define least-significant bit from 2-bit encoder.
+	bool MSB = gpioRead(the_encoder->channel_a);	//Define most-significant bit from 2-bit encoder.
+	bool LSB = gpioRead(the_encoder->channel_b);	//Define least-significant bit from 2-bit encoder.
 
-	int code = (MSB << 1) | LSB;				//Define 2-bit value read from encoder.
-	int sum = (the_encoder->last_code << 2) | code;		//Define 4-bit value: 2 msb is the previous value from the encoder and 2 lsb is the current.
+	int code = (MSB << 1) | LSB;			//Define 2-bit value read from encoder.
+	int sum = (the_encoder->last_code << 2) | code;	//Define 4-bit value: 2 msb is the previous value from the encoder and 2 lsb is the current.
 
 	//Use the following two lines to register every single pulse.
 	//That's 4 pulses per detent on ADA377 encoder.
@@ -92,8 +94,8 @@ void volume_ISR(int32_t gpio, int32_t level, uint32_t tick)
 
 	//Use the following two lines to register only every fourth pulse.
 	//That's equivalent to a single pulse for every detent on ADA377 encoder.
-	if (sum == 0b0100 || sum == 0b1011) {the_encoder->volume_delta++;}	//Turned clockwise.
-	if (sum == 0b0111 || sum == 0b1000) {the_encoder->volume_delta--;}	//Turned counter-clockwise.
+	if (sum == 0b0100 || sum == 0b1011) the_encoder->volume_delta++;	//Turned clockwise.
+	if (sum == 0b0111 || sum == 0b1000) the_encoder->volume_delta--;	//Turned counter-clockwise.
 
 	the_encoder->last_code = code;	//Update value of last_code for comparison the next time this ISR is triggered.
 }
@@ -105,8 +107,8 @@ void toggle_ISR(int32_t gpio, int32_t level, uint32_t tick)
 {
 	if (((tick - last_button_trigger) > DEBOUNCE_US) && (gpioRead(TOGGLE_PIN) == HIGH))
 	{
-		if (block_toggle)	{block_toggle = FALSE;}		//Ignore the toggle if the button was used to send a stop signal.
-		else			{toggle_signal = TRUE;}		//Flag toggle with a normal press.
+		if (block_toggle)	block_toggle = FALSE;		//Ignore the toggle if the button was used to send a stop signal.
+		else			toggle_signal = TRUE;		//Flag toggle with a normal press.
 
 		last_button_trigger = tick;				//Reset the de-bounce timestamp.
 	}
@@ -147,8 +149,8 @@ void update_volume(void)
 //Triggered in the main loop by polling the toggle flag being set (by ISR).
 void update_toggle(void)
 {	
-	if (get_mpd_status() == MPD_STATE_PLAY)	{mpd_run_pause(connection, TRUE);}	//Currently playing, so pause.
-	else					{mpd_run_play(connection);}		//Currently paused or stopped, so play.
+	if (get_mpd_status() == MPD_STATE_PLAY)	mpd_run_pause(connection, TRUE);	//Currently playing, so pause.
+	else					mpd_run_play(connection);		//Currently paused or stopped, so play.
 	toggle_signal = FALSE;
 }
 
@@ -156,21 +158,21 @@ void update_toggle(void)
 //Initialise the hardware inputs - two channels on the rotary encoder and a push-button.
 bool init_hardware(void)
 {
-	if (gpioInitialise() == PI_INIT_FAILED) {return(FALSE);}				//Initialise gpio pins on raspi, return false if failure.
+	if (gpioInitialise() == PI_INIT_FAILED) return(FALSE);				//Initialise gpio pins on raspi, return false if failure.
 
-	if (gpioSetMode(TOGGLE_PIN, PI_INPUT) != 0) {return(FALSE);}				//Set as input the pin to which the toggle button is connected.
-	if (gpioSetPullUpDown(TOGGLE_PIN, PI_PUD_UP) != 0) {return(FALSE);}			//Enable internal pull-up resistor on the toggle pin.
-	if (gpioSetISRFunc(TOGGLE_PIN, RISING_EDGE, 0, &toggle_ISR) != 0) {return(FALSE);}	//Set up interrupt on the toggle button.
+	if (gpioSetMode(TOGGLE_PIN, PI_INPUT)) return(FALSE);				//Set as input the pin to which the toggle button is connected.
+	if (gpioSetPullUpDown(TOGGLE_PIN, PI_PUD_UP)) return(FALSE);			//Enable internal pull-up resistor on the toggle pin.
+	if (gpioSetISRFunc(TOGGLE_PIN, RISING_EDGE, 0, &toggle_ISR)) return(FALSE);	//Set up interrupt on the toggle button.
 
-	the_encoder = init_encoder(VOL_ENCODER_A_PIN, VOL_ENCODER_B_PIN);			//Initialise values stored in global struct "the_encoder".
-	if (the_encoder == NULL) {return(FALSE);}						//Return false if initialisation fails.
+	the_encoder = init_encoder(VOL_ENCODER_A_PIN, VOL_ENCODER_B_PIN);		//Initialise values stored in global struct "the_encoder".
+	if (the_encoder == NULL) return(FALSE);						//Return false if initialisation fails.
 
 	return(TRUE);
 }
 
 
 //Main program loop.  arguments (argv[]) and number of arguments (argc) ignored.
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	////////////////////////////////////////////////////////////////////////////////
 	////////	This initial code in main required for daemonisation	////////
@@ -180,15 +182,15 @@ int main(int argc, char* argv[])
 	pid_t sid = 0;	//Initialise sis (session id).
 
 	pid = fork();					//Fork off from the parent process.  fork() should return PID of child process.
-	if (pid < 0) {exit(EXIT_FAILURE);}		//If fork returns a value less than 0 then an error has occured.
-	if (pid > 0) {exit(EXIT_SUCCESS);}		//if fork returns a positive value then we have successfully forked a child process from the parent process.
+	if (pid < 0)		exit(EXIT_FAILURE);	//If fork returns a value less than 0 then an error has occured.
+	if (pid > 0)		exit(EXIT_SUCCESS);	//if fork returns a positive value then we have successfully forked a child process from the parent process.
 
 	umask(0);					//Unmask the file mode - allow the daemon to open, read and write any file anywhere.
 
 	sid = setsid();					//Set new session and allocate session id.  if successful, child process is now process group leader.
-	if(sid < 0) {exit(EXIT_FAILURE);}		//If setsid failed, exit.
+	if (sid < 0)		exit(EXIT_FAILURE);	//If setsid failed, exit.
 
-	if ((chdir("/")) < 0) {exit(EXIT_FAILURE);} 	//Change working directory. If we can't find the directory we exit with failure.
+	if ((chdir("/")) < 0)	exit(EXIT_FAILURE);	//Change working directory. If we can't find the directory we exit with failure.
 
 	//Daemons should not involve any standard user interaction, so close stdin, stdout and stderr.
 	close(STDIN_FILENO);
@@ -199,9 +201,8 @@ int main(int argc, char* argv[])
 	////////	The Payload - at this point, the programme is running as a daemon	////////
 	////////////////////////////////////////////////////////////////////////////////////////////////
 
-	if (init_hardware() != TRUE) {exit(EXIT_FAILURE);}	//Initialise the hardware (encoder and push button).
-	if (init_mpd() != TRUE) {exit(EXIT_FAILURE);}		//Initialise mpd connection.
-
+	if (!init_hardware())	exit(EXIT_FAILURE);	//Initialise the hardware (encoder and push button).
+	if (!init_mpd())	exit(EXIT_FAILURE);	//Initialise mpd connection.
 
 	//The main infinite loop.
 	while (TRUE)
@@ -209,9 +210,9 @@ int main(int argc, char* argv[])
 		get_mpd_status();						//Loop until valid connection state. Keeps the connection alive.
 		gpioDelay(IDLE_DELAY);						//A delay so as to not max out cpu usage when running the main loop.
 
-		if (the_encoder->volume_delta)	{update_volume();}		//Poll for a change in the encoder value.  If so, run update_volume().
-		if (toggle_signal)		{update_toggle();}		//Poll the state of the toggle_signal flag.  If set, run update_toggle().
-		if (toggle_long_press())	{mpd_run_stop(connection);}	//Poll for a long hold of the button.  If so, send the stop command.
+		if (the_encoder->volume_delta)	update_volume();		//Poll for a change in the encoder value.  If so, run update_volume().
+		if (toggle_signal)		update_toggle();		//Poll the state of the toggle_signal flag.  If set, run update_toggle().
+		if (toggle_long_press())	mpd_run_stop(connection);	//Poll for a long hold of the button.  If so, send the stop command.
 	}
 
 	return(-1);	//Never reached.
