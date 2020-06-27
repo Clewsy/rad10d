66272e533b9baf0ead6fbc35480bddf14431dd45
clewsy <jadon@clews.pro>
2020-06-27 17:13:22 +1000

    Re-structured for consistency and readability.

--- rad10d.c
+++ rad10d.c
@@ -57,21 +57,32 @@ uint8_t get_mpd_status(void)
 }
 
 
-//Function to initialise the struct that reflects the state of the encoder.
-struct encoder *init_encoder(uint8_t channel_a, uint8_t channel_b)
+//Function to initialise the struct that reflects the state of the encoder hardware.
+struct encoder *init_encoder(uint8_t channel_a, uint8_t channel_b, uint8_t button_pin)
 {
 	the_encoder = malloc(sizeof(struct encoder));	//Allocates heap memory for globally accessible "the_encoder".
 
-	the_encoder->channel_a = channel_a;	//Define the encoder channel a pin.
-	the_encoder->channel_b = channel_b;	//Define the encoder channel b pin.
-	the_encoder->last_code = 0;		//Initialise the last encoder code (for comparing to current code).
-	the_encoder->volume_delta = 0;		//Initialise the value of desired volume change.
+	the_encoder->channel_a = channel_a;		//Define the encoder channel a hardware pin.
+	the_encoder->channel_b = channel_b;		//Define the encoder channel b hardware pin.
+	the_encoder->last_code = 0;			//Initialise the last encoder code (for comparing to current code).
+	the_encoder->volume_delta = 0;			//Initialise the value of desired volume change.
 
-	if (gpioSetPullUpDown(channel_a, PI_PUD_UP)) return(NULL);	//Enable pull-up resistor on channel a pin.
-	if (gpioSetPullUpDown(channel_b, PI_PUD_UP)) return(NULL);	//Enable pull-up resistor on channel b pin.
+	the_encoder->button_pin = button_pin;		//Define the push-button hardware pin.
+	the_encoder->button_pressed_time = 0;		//Initialise the time when a button press (gone low) event is detected and validated.
+	the_encoder->button_released_time = 0;		//Initialise the time when a button release (gone high) event is detected and validated.
+	the_encoder->button_signal = SIGNAL_NULL;	//Initialise the control signal which is set based on button state (quick press to toggle, long press to stop).
 
-	if (gpioSetISRFunc(channel_a, EITHER_EDGE, 100, &volume_ISR)) return(NULL);	//Enable hardware interrupt input on encoder channel a pin and define routine to run.
-	if (gpioSetISRFunc(channel_b, EITHER_EDGE, 100, &volume_ISR)) return(NULL);	//Enable hardware interrupt input on encoder channel b pin and define routine to run.
+	if (gpioSetMode(channel_a, PI_INPUT)) return(FALSE);	//Set as input the pin to which the encoder channel a is connected.
+	if (gpioSetMode(channel_a, PI_INPUT)) return(FALSE);	//Set as input the pin to which the encoder channel b is connected.
+	if (gpioSetMode(button_pin, PI_INPUT)) return(FALSE);	//Set as input the pin to which the push-button is connected.
+
+	if (gpioSetPullUpDown(channel_a, PI_PUD_UP)) return(FALSE);	//Enable pull-up resistor on channel a pin.
+	if (gpioSetPullUpDown(channel_b, PI_PUD_UP)) return(FALSE);	//Enable pull-up resistor on channel b pin.
+	if (gpioSetPullUpDown(button_pin, PI_PUD_UP)) return(FALSE);	//Enable pull-up resistor on push-button pin.
+
+	if (gpioSetISRFunc(channel_a, EITHER_EDGE, 100, &volume_ISR)) return(FALSE);	//Enable hardware interrupt input on encoder channel a pin and define routine to run.
+	if (gpioSetISRFunc(channel_b, EITHER_EDGE, 100, &volume_ISR)) return(FALSE);	//Enable hardware interrupt input on encoder channel b pin and define routine to run.
+	if (gpioSetISRFunc(button_pin, EITHER_EDGE, 0, &button_ISR)) return(FALSE);	//Enable hardware interrupt input on push-button pin and define routine to run.
 
 	return(the_encoder);	//Return address of "*the_encoder".
 }
@@ -102,56 +113,59 @@ void volume_ISR(int32_t gpio, int32_t level, uint32_t tick)
 
 
 //Interrupt subroutine to be triggered by pressing the toggle button.
-//The gpioSetISRFunc requires ISRs such as this to receive gpio, level and tick.
-void toggle_ISR(int32_t gpio, int32_t level, uint32_t tick)
+//The gpioSetISRFunc requires ISRs such as this to receive gpio, level and tick (renamed time).
+void button_ISR(int32_t gpio, int32_t level, uint32_t time)
 {
-	if (((tick - last_button_trigger) > DEBOUNCE_US) && (gpioRead(TOGGLE_PIN) == HIGH))
-	{
-		if (block_toggle)	block_toggle = FALSE;		//Ignore the toggle if the button was used to send a stop signal.
-		else			toggle_signal = TRUE;		//Flag toggle with a normal press.
+	if (gpioRead(the_encoder->button_pin) == LOW) the_encoder->button_pressed_time = time;
 
-		last_button_trigger = tick;				//Reset the de-bounce timestamp.
+
+	if ((gpioRead(the_encoder->button_pin) == HIGH) && ((time - the_encoder->button_released_time) > DEBOUNCE_US))
+	{
+		if (the_encoder->button_signal == SIGNAL_STOP)	the_encoder->button_signal = SIGNAL_NULL;	//Clear stop signal.
+		else						the_encoder->button_signal = SIGNAL_TOGGLE;	//Or set toggle signal.
+
+		the_encoder->button_released_time = time;	//Reset the de-bounce timestamp.
 	}
+
+
 }
 
 
 //Function returns true if the toggle button has been held down for a specified duration (TOGGLE_BUTTON_LONG_PRESS microseconds).
-bool toggle_long_press(void)
+void poll_long_press(struct encoder *enc)
 {
-	if (gpioRead(TOGGLE_PIN) == LOW)						//If the button is pressed.
+	if ((gpioRead(enc->button_pin) == LOW) && ((gpioTick() - enc->button_pressed_time) > TOGGLE_BUTTON_LONG_PRESS)) 
 	{
-		if (toggle_button_held == FALSE)					//If this is the first iteration when the button has been pressed.
-		{
-			time_when_pressed = gpioTick();					//Record the time the press was first flagged.
-			toggle_button_held = TRUE;					//Set the flag so that the next iteration doesn't reset the timer.
-		}
 
-		if ((gpioTick() - (time_when_pressed) > TOGGLE_BUTTON_LONG_PRESS))	//If the button is held for duration exceeding TOGGLE_BUTTON_LONG_PRESS
-		{
-			block_toggle = TRUE;						//Set the stop flag (so button ISR ignores when the button is released).
-			return(TRUE);							//Return true: button long press true.
-		}
+		enc->button_signal = SIGNAL_STOP;
 	}
-	else {toggle_button_held = FALSE;}						//Clear the flag that indicates the button is being held.
 
-	return(FALSE);									//Return false: button long press false.
 }
 
 
 //Triggered in the main loop by polling for a change in the encoder value.
-void update_volume(void)
+void update_mpd_volume(volatile int8_t *delta_address)
 {
-	mpd_run_change_volume(connection, the_encoder->volume_delta);	//Change the volume.
-	the_encoder->volume_delta = 0;					//Clear the desired volume delta.
+	mpd_run_change_volume(connection, *delta_address);	//Change the volume.
+	*delta_address = 0;					//Clear the desired volume delta.
 }
 
 
 //Triggered in the main loop by polling the toggle flag being set (by ISR).
-void update_toggle(void)
+void update_mpd_state(volatile uint8_t *signal_address)
 {	
-	if (get_mpd_status() == MPD_STATE_PLAY)	mpd_run_pause(connection, TRUE);	//Currently playing, so pause.
-	else					mpd_run_play(connection);		//Currently paused or stopped, so play.
-	toggle_signal = FALSE;
+	switch(*signal_address)
+	{
+		case SIGNAL_TOGGLE:
+			if (get_mpd_status() == MPD_STATE_PLAY)	mpd_run_pause(connection, TRUE);	//Currently playing, so pause.
+			else					mpd_run_play(connection);		//Currently paused or stopped, so play.
+			*signal_address = SIGNAL_NULL;							//Reset mpd state control signal.
+			break;
+		case SIGNAL_STOP:
+			mpd_run_stop(connection);	//Send the stop command.
+			break;				//Do not reset signal, used as a check in the button ISR.
+	}
+
 }
 
 
@@ -160,11 +174,7 @@ bool init_hardware(void)
 {
 	if (gpioInitialise() == PI_INIT_FAILED) return(FALSE);				//Initialise gpio pins on raspi, return false if failure.
 
-	if (gpioSetMode(TOGGLE_PIN, PI_INPUT)) return(FALSE);				//Set as input the pin to which the toggle button is connected.
-	if (gpioSetPullUpDown(TOGGLE_PIN, PI_PUD_UP)) return(FALSE);			//Enable internal pull-up resistor on the toggle pin.
-	if (gpioSetISRFunc(TOGGLE_PIN, RISING_EDGE, 0, &toggle_ISR)) return(FALSE);	//Set up interrupt on the toggle button.
-
-	the_encoder = init_encoder(VOL_ENCODER_A_PIN, VOL_ENCODER_B_PIN);		//Initialise values stored in global struct "the_encoder".
+	the_encoder = init_encoder(VOL_ENCODER_A_PIN, VOL_ENCODER_B_PIN, BUTTON_PIN);	//Initialise values stored in global struct "the_encoder".
 	if (the_encoder == NULL) return(FALSE);						//Return false if initialisation fails.
 
 	return(TRUE);
@@ -179,7 +189,7 @@ int main(int argc, char *argv[])
 	////////////////////////////////////////////////////////////////////////////////
 
 	pid_t pid = 0;	//Initialise pid (process id).
-	pid_t sid = 0;	//Initialise sis (session id).
+	pid_t sid = 0;	//Initialise sid (session id).
 
 	pid = fork();					//Fork off from the parent process.  fork() should return PID of child process.
 	if (pid < 0)		exit(EXIT_FAILURE);	//If fork returns a value less than 0 then an error has occured.
@@ -207,12 +217,12 @@ int main(int argc, char *argv[])
 	//The main infinite loop.
 	while (TRUE)
 	{
-		get_mpd_status();						//Loop until valid connection state. Keeps the connection alive.
-		gpioDelay(IDLE_DELAY);						//A delay so as to not max out cpu usage when running the main loop.
+		get_mpd_status();	//Loop until valid connection state. Keeps the connection alive.
+		gpioDelay(IDLE_DELAY);	//A delay so as to not max out cpu usage when running the main loop.
 
-		if (the_encoder->volume_delta)	update_volume();		//Poll for a change in the encoder value.  If so, run update_volume().
-		if (toggle_signal)		update_toggle();		//Poll the state of the toggle_signal flag.  If set, run update_toggle().
-		if (toggle_long_press())	mpd_run_stop(connection);	//Poll for a long hold of the button.  If so, send the stop command.
+		poll_long_press(the_encoder);							//Poll for a long hold of the button.
+		if (the_encoder->volume_delta)	update_mpd_volume(&the_encoder->volume_delta);	//Poll for a change in the encoder value.  If so, run update_volume().
+		if (the_encoder->button_signal)	update_mpd_state(&the_encoder->button_signal);	//Poll the state of the toggle_signal flag.  If set, run update_toggle().
 	}
 
 	return(-1);	//Never reached.
--- rad10d.h
+++ rad10d.h
@@ -21,11 +21,20 @@
 //Hardware definitions.  Pinout numbering for use with the pigpio is the same as the Broadcom chip numbering (note, != wiringPi numbering).
 #define VOL_ENCODER_A_PIN		14	//Encoder channel A.
 #define VOL_ENCODER_B_PIN		15	//Encoder channel B.
-#define TOGGLE_PIN			18	//Play/pause toggle button.
+#define BUTTON_PIN			18	//Play/pause toggle button.
 #define LOW				0	//Pin grounded (e.g. button pressed).
 #define HIGH				1	//Pin pulled high.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Timer duration definitions.
 #define DEBOUNCE_US			100000	//Debounce value for toggle button (in microseconds).
 #define TOGGLE_BUTTON_LONG_PRESS	2000000	//Long press duration (in microseconds).
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//mpd control definitions.
+#define SIGNAL_NULL			0	//mpd control signal set by the push-button.  SIGNAL_NULL:	No command.
+#define SIGNAL_TOGGLE			1	//mpd control signal set by the push-button.  SIGNAL_TOGGLE:	Send the toggle command.
+#define SIGNAL_STOP			2	//mpd control signal set by the push-button.  SIGNAL_STOP:	Send the stop command.
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //mpd connection definitions.
@@ -34,29 +43,27 @@
 #define MPD_TIMEOUT	1000		//Connection timeout (in ms).
 #define IDLE_DELAY	10000		//Delay within the main loop to reduce cpu load (in microseconds).
 
-struct mpd_connection *connection = NULL;	//Initialise globally accessible structure contatining mpd connection info (refer "mpd/client.h").
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Global variable declarations.
+//Global declarations.
+
+struct mpd_connection *connection = NULL;	//Initialise globally accessible structure contatining mpd connection info (refer "mpd/client.h").
 
 //Define the structure that represents data from the encoder.
 struct encoder
 {
-	uint8_t channel_a;		//Hardware pin to which encoder A channel is connected.
-	uint8_t channel_b;		//Hardware pin to which encoder B channel is connected.
-	volatile uint8_t last_code;	//Last code detected by the encoder.  Compare with current code to determine direction of rotation.
-	volatile int8_t volume_delta;	//Positive or negative value depending on direction encoder was turned.  Represents desired volume delta.
+	uint8_t channel_a;			//Hardware pin to which encoder A channel is connected.
+	uint8_t channel_b;			//Hardware pin to which encoder B channel is connected.
+	volatile uint8_t last_code;		//Last code detected by the encoder.  Compare with current code to determine direction of rotation.
+	volatile int8_t volume_delta;		//Positive or negative depending on direction encoder was turned.  Represents desired volume delta.
+
+	uint8_t button_pin;			//Hardware pin to which the push-buttonis connected.
+	volatile uint32_t button_pressed_time;	//Timestamp set whenever a button press is detected and validated (de-bounced).
+	volatile uint32_t button_released_time;	//Timestamp set whenever a button release is detected and validated (de-bounced).
+	volatile uint8_t button_signal;		//mpd control signal set by the push button.  SIGNAL_NULL, SIGNAL_TOGGLE or SIGNAL_STOP.
 };
 
-//Declare structure named the_encoder - gloablly accessible (used in ISR).
-struct encoder *the_encoder;
+struct encoder *the_encoder;			//Declare structure named the_encoder - gloablly accessible (used in ISR).
 
-//Declare and initialise the variables referenced in the button toggle ISR.
-bool toggle_signal = FALSE;		//Flag indicates whether or not a button press has been registered.
-bool block_toggle = FALSE;		//Flag indicates a long press of the toggle button, so block toggling when the button is released.
-bool toggle_button_held = FALSE;	//Flag indicates whether or not the last loop iteration determined the toggle button was held down. 
-uint32_t last_button_trigger = 0;	//Timer value (microseconds) used as a reference to debounce button press.
-uint32_t time_when_pressed = 0;		//Initialise the time stamp used to detect a long-press of the toggle button.
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Function declarations.
@@ -64,10 +71,10 @@ static struct mpd_connection *setup_connection(void);
 bool init_mpd(void);
 bool mpd_reconnect(void);
 uint8_t get_mpd_status(void);
-struct encoder *init_encoder(uint8_t channel_a, uint8_t channel_b);
+struct encoder *init_encoder(uint8_t channel_a, uint8_t channel_b, uint8_t toggle_pin);
 void volume_ISR(int32_t gpio, int32_t level, uint32_t tick);
-void toggle_ISR(int32_t gpio, int32_t level, uint32_t tick);
-bool toggle_long_press(void);
-void update_volume(void);
-void update_toggle(void);
+void button_ISR(int32_t gpio, int32_t level, uint32_t tick);
+void poll_long_press(struct encoder *enc);
+void update_mpd_volume(volatile int8_t *delta_adddress);
+void update_mpd_state(volatile uint8_t *signal_address);
 bool init_hardware(void);
